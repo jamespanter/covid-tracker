13e893fa0e3ed59a29f7b392b924522f39915afd
jamespanter <james.panter@blueyonder.co.uk>
2020-04-11 22:42:03 +0100

    Styling

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,30 +5,33 @@ const Cards = (props) => {
   const { data } = props;
 
   return (
-    <section className={styles.cards}>
-      <div className={styles.cases}>
-        <h2>Cases</h2>
-        <p>
-          {data.confirmed.value
-            .toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-        </p>
-      </div>
-      <div className={styles.recovered}>
-        <h2>Recovered</h2>
-        <p>
-          {data.recovered.value
-            .toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-        </p>
-      </div>
-      <div className={styles.deaths}>
-        <h2>Deaths</h2>
-        <p>
-          {data.deaths.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-        </p>
-      </div>
-    </section>
+    <>
+      <h2>Global Stats: </h2>
+      <section className={styles.cards}>
+        <div className={styles.cases}>
+          <h2>Cases</h2>
+          <p>
+            {data.confirmed.value
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
+        </div>
+        <div className={styles.recovered}>
+          <h2>Recovered</h2>
+          <p>
+            {data.recovered.value
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
+        </div>
+        <div className={styles.deaths}>
+          <h2>Deaths</h2>
+          <p>
+            {data.deaths.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
